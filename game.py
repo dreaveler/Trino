4970da53a8015fec85f4db4c6aa0b9322943147c
dreaveler <2169448673@qq.com>
2025-07-07 11:29:37 +0800

    update player game

--- game.py
+++ game.py
@@ -6,10 +6,16 @@ class Game:
     def __init__(self,player_num:int):
         self.player_num = player_num
 
-        self.playere_list: List[Player] = []
+        self.player_list: List[Player] = []
         self.unocard_pack:List[UnoCard] = []
         self.unocard_discard_pile:List[UnoCard] = []
 
     def create_unocard_pack(self):
         pass
     #需要写一套uno牌
+    #将玩家加对对局中同时将game赋给玩家
+    def add_player(self,player:Player):
+        self.player_list.append(player)
+        player.game = self
+
+    
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 from card import UnoCard, MRCard
 from typing import List, Callable
 from enum import Enum
+from game import Game
 
 class State(Enum):
     playing = 0
@@ -14,3 +15,23 @@ class Player:
         self.uno_list: List[UnoCard] = []
         self.mr_card: MRCard = None
         self.state: State = State.playing
+        self.game : Game = None
+    #拿num张牌,先用print来做游戏提示了
+    def get_card(self,num:int):
+        for _ in range(num):
+            if not self.game.unocard_pack:
+                self.uno_list.append(self.game.unocard_pack.pop())
+            else:
+                print("uno牌已被拿完")
+    #检查出牌是否合规 未完成  需要写
+    def check_card(self,card):
+        pass
+    #出牌 如果出牌合规则回合结束 否则无视发生     需要一个flag来说明回合结束?
+    def play_a_hand(self,location):
+        card = self.uno_list.pop(location)
+        if self.check_card(card):
+            #这里用lmt写的数据结构 
+            pass
+        else:
+            print("出牌不符合规范！")
+            pass
