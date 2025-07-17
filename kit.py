0156a3a6b501b0de3cef48f89224a0c6ca6d2d01
floatup <2400013081@stu.pku.edu.cn>
2025-07-17 22:21:28 +0800

    加了一些技能做实验，将judge改为kit，这个文件将用于锦囊牌的编写

--- kit.py
+++ kit.py
@@ -1,5 +1,6 @@
 from card import UnoCard
 from player import Player
+from game import Game
 
 #本文件将编写一些判定牌的逻辑
 class Judge:
@@ -29,3 +30,19 @@ class Lightening(Judge):
             player.get_card(10)
         else:
             self.transfer=True
+
+class Duel:
+    def __init__(self,color,player1,player2):
+        self.color=color
+        self.player1=player1
+        self.player2=player2
+        self.duel_cards=[]
+    def __call__(self):
+        pass#没想好如何让双方轮流出牌
+
+class NanManRuQin:
+    def __call__(self, caller:Player, game:Game):
+        for player in game.player_list:
+            if player!=caller:
+                #和决斗一样没想好如何让大家轮流出牌并给出一个bool,这里应该有一个if
+                    player.get_card(4)
